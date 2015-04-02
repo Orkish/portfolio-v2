4d7154c537ffdb9c921085d993a53c759dc04b84
Winson <winsonyuan1@gmail.com>
2015-04-02 14:36:10 -0400

    created node server to test angular app locally

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require( 'express' )
+		,app = express()
+		,path = require( 'path' );
+
+// set up public folder to serve public assets
+app.use( express.static( __dirname + '/public' ));
+
+// set up one route to index.html file 
+app.get( '*', function( req, res ) {
+	res.sendFile( path.join( __dirname + '/public/index.html' ));
+});
+
+// starting the server 8080
+app.listen(1337);
+console.log( 'Magical shit happening on 1337' );
