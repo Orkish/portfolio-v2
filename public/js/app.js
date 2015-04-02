a177b78619ec1b130d24022ef3e1e6ec1f7acf38
Winson <winsonyuan1@gmail.com>
2015-04-02 18:56:01 -0400

    updated js file

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,27 @@
 var app = angular.module( 'mainApp', [ 'routerRoutes', 'ngAnimate' ]);
 
 app.controller( 'mainController', function() {
-	var vm = this;
-	vm.welcomeMsg = 'Welcome to home page';
-});
+		var vm = this;
+		vm.welcomeMsg = 'Welcome to home page';
+	})
+
+	// about controller 
+	.controller( 'aboutController', function() {
+
+	})
+
+	// work controller
+	.controller( 'workController', function() {
+
+	})
+
+	// hobbies controller
+	.controller( 'hobbiesController', function() {
+
+	})
+
+	// contact controller 
+	.controller( 'contactController', function() {
+
+	});
 
